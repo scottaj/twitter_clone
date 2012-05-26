b612b180febd8834b4218bcb61c35a73a9f0506a
Al Scott <al.scott@atomicobject.com>
2012-05-25 22:09:39 -0400

    Fleshed out User class. Added methods to follow and unfollow another user.

--- lib/user.rb
+++ lib/user.rb
@@ -1,7 +1,18 @@
 
 
 class User
-  def initialize()
+  def initialize(handle)
+    @handle = handle
+    @following = []
+  end
 
+  attr_reader :handle
+
+  def follow_user(user)
+    @following << user.handle
+  end
+  
+  def unfollow_user(user)
+    @following.delete_if {|handle| handle == user.handle}
   end
 end
--- main.rb
+++ main.rb
@@ -8,5 +8,4 @@ def routing_table()
   
 end
 
-
 routing_table if __FILE__ == $0
