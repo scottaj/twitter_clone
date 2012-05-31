3685e88eba130c7c3ccdba2f3d620c5b4a082fd1
Al Scott <al.scott@atomicobject.com>
2012-05-31 16:17:21 -0400

    Added configuration to sinatra for DB connection.

--- main.rb
+++ main.rb
@@ -10,12 +10,32 @@ require_relative 'lib/tweet_model'
 
 
 class TwitterClone < Sinatra::Base
+
+  configure do
+    enable :sessions
+    
+    db_settings = YAML::load(File.open('./db.conf', 'r'))
+
+    if db_settings[:hostname] or db_settings[:port]
+      if db_settings[:port]
+        db_conn = Mongo::Connection.new(db_settings[:hostname], db_settings[:port]).db(db_settings[:database])
+      else
+        db_conn = Mongo::Connection.new(db_settings[:hostname]).db(db_settings[:database])
+      end
+    else
+      db_conn = Mongo::Connection.new.db(db_settings[:database])
+    end
+
+    db_conn.authenticate(db_settings[:username], db_settings[:password]) if db_settings[:username]
+
+    @user_model = UserModel.new(db_conn, db_settings[:user_collection])
+    @tweet_model = TweetModel.new(db_conn, db_settings[:tweet_collection])
+  end
   
   # Main routing table for application.
   get '/' do
-    @page_title = "Home"
-
-    slim :index
+    test = "Home"
+    slim :index, locals: {page_title: test}
   end
 end
 
