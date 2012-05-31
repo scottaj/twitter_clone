ae64ab2a74d2f87a4c8c52b67df7081147194a8f
Al Scott <al.scott@atomicobject.com>
2012-05-31 13:29:32 -0400

    Replaced rakefile.

--- main.rb
+++ main.rb
@@ -1,13 +1,22 @@
 require 'bundler/setup'
 require 'sinatra'
+require 'slim'
+require 'mongo'
+require 'yaml'
+require_relative 'lib/user'
+require_relative 'lib/tweet'
+require_relative 'lib/user_model'
+require_relative 'lib/tweet_model'
 
 
-class TwitterClone< Sinatra::Base
+class TwitterClone < Sinatra::Base
+  
   # Main routing table for application.
   get '/' do
-    
+    @page_title = "Home"
+
+    slim :index
   end
 end
 
-
-TwitterClone.run if __FILE__ == $0
+TwitterClone.run! if __FILE__ == $0
